16091d0bb97b1427eb038456b58c829e64cce89b
Christer Miroy <chrstrzm.mry@gmail.com>
2024-03-06 00:41:36 +0800

    with infinite scroll functionality

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
+async function scrapeDataFromPage(page) {
+    const data = await page.evaluate(() => {
+        const root = Array.from(document.querySelectorAll('.product-item'));
+        let id = 0;
+        const products = root.map(product => ({
+            id: ++id,
+            name: product.querySelector('.product-item__title').textContent,
+            price: product.querySelector('.money').textContent,
+            imageURL: product.querySelector('img').src
+        }));
+
+        return products;
+    });
+
+    // data validation
+    if (!Array.isArray(data) || data.length === 0) {
+        throw new Error('Data not found');
+    }
+
+    return data;
+}
+
+async function scrollInfinitely(page) {
+    let data = [];
+    let scrolling = true;
+    while (scrolling) {
+        const newData = await scrapeDataFromPage(page);
+        data = data.concat(newData);
+        scrolling = await page.evaluate(() => {
+            const scrollHeight = document.documentElement.scrollHeight;
+            window.scrollBy(0, scrollHeight);
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(window.innerHeight + window.scrollY >= scrollHeight);
+                }, 1000);
+            });
+        });
+    }
+    return data;
+}
+
 async function main() {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://ecommerce.datablitz.com.ph/collections/pc-mac?page=2&pf_t_categories=Controllers');
+        await page.goto('https://ecommerce.datablitz.com.ph/collections/pc-mac?pf_t_categories=Laptops');
 
-        data = await page.evaluate(() => {
-            root = Array.from(document.querySelectorAll('.product-item'));
-            id = 0;
-            products = root.map(product => ({
-                id: ++id,
-                name: product.querySelector('.product-item__title').textContent,
-                price: product.querySelector('.money').textContent,
-                imageURL: product.querySelector('img').src
-            }));
-
-            return products;
-        });
-
-        // data validation
-        if (!Array.isArray(data) || data.length === 0) {
-            throw new Error('Data not found');
-        }
+        const data = await scrollInfinitely(page);
 
         console.log(data);
 
