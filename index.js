e68656b2d38dd42eccb1052e31cd091b9f777a67
Christer Miroy <chrstrzm.mry@gmail.com>
2024-03-05 23:52:55 +0800

    process.exit to terminate node if error exist

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-    const fs = require('fs');
-    const puppeteer = require('puppeteer');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
-    async function main() {
-        try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto('https://ecommerce.datablitz.com.ph/collections/pc-mac?page=2&pf_t_categories=Controllers');
+async function main() {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://ecommerce.datablitz.com.ph/collections/pc-mac?page=2&pf_t_categories=Controllers');
 
-            data = await page.evaluate(() => {
-                root = Array.from(document.querySelectorAll('.product-item'));
-                id = 0;
-                products = root.map(product => ({
-                    id: ++id,
-                    name: product.querySelector('.product-item__title').textContent,
-                    price: product.querySelector('.money').textContent,
-                    imageURL: product.querySelector('img').src
-                }));
+        data = await page.evaluate(() => {
+            root = Array.from(document.querySelectorAll('.product-item'));
+            id = 0;
+            products = root.map(product => ({
+                id: ++id,
+                name: product.querySelector('.product-item__title').textContent,
+                price: product.querySelector('.money').textContent,
+                imageURL: product.querySelector('img').src
+            }));
 
-                return products;
-            });
+            return products;
+        });
 
-            // data validation
-            if (!Array.isArray(data) || data.length === 0) {
-                throw new Error('Data not found');
-            }
-
-            console.log(data);
-
-            // Write data to JSON file
-            fs.writeFile('products.json', JSON.stringify(data), (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log('JSON data is saved.');
-            });
-
-            await browser.close();
-        } catch (error) {
-            console.log('Error has occurred: ', error.message);
+        // data validation
+        if (!Array.isArray(data) || data.length === 0) {
+            throw new Error('Data not found');
         }
-    }
 
-    main();
+        console.log(data);
+
+        // Write data to JSON file
+        fs.writeFile('products.json', JSON.stringify(data), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('JSON data is saved.');
+        });
+
+        await browser.close();
+    } catch (error) {
+        console.log('Error has occurred: ', error.message);
+        process.exit(1);
+    }
+}
+
+main();
